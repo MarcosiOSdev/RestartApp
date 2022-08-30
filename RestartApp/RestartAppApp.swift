1620c82d7d03ec57c07eabfbb38334e522568041
Marcos Felipe <mfelipesp@gmail.com>
2022-08-30 15:56:28 -0300

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by marcos.felipe.souza on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
